d0563662c00c52666ab1e5cb2eafdb66e63d21d5
oliveiraxand <alexandre.osantos253@gmail.com>
2023-10-24 18:43:31 -0300

    feat: requisito 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,22 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # n = len(word)
+    # if n == 1:
+    #     return True
+    if word == '':
+        return False
+    if word[low_index] == word[high_index]:
+        if high_index <= 1:
+            return True
+        low = low_index + 1
+        high = high_index - 1
+        return is_palindrome_recursive(word, low, high)
+        # return is_palindrome_recursive(word, low_index=low_index+1,
+        # high_index=high_index-1)
+    return False
+    # if low_index == len(word):
+    #     return True
+    # is_palindrome_recursive(word, )
+    # raise NotImplementedError
+# O
+# V
+# O
