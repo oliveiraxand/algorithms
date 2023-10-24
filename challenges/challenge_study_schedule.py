9704a201899653f707633270588c14475d5d30ba
oliveiraxand <alexandre.osantos253@gmail.com>
2023-10-24 16:51:06 -0300

    fix: lint or flake8

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,6 +1,6 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    if target_time == None:
+    if target_time is None:
         return None
     counter = 0
     for pp in permanence_period:
@@ -9,6 +9,4 @@ def study_schedule(permanence_period, target_time):
         if pp[0] <= target_time <= pp[1]:
             counter += 1
     return counter
-    # raise NotImplementedError
-
-study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 5)
+    raise NotImplementedError
