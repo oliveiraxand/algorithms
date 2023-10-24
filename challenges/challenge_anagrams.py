425ab72812834e0131fed7100dda719a04a2dc9e
oliveiraxand <alexandre.osantos253@gmail.com>
2023-10-24 19:40:01 -0300

    feat: requisito 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,48 @@
+def sortword(word, start=0, end=None):
+    if end is None:
+        end = len(word)
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        sortword(word, start, end=mid)
+        sortword(word, start=mid, end=end)
+        merge(word, start, mid, end)
+    return word
+
+
+def merge(word, start, mid, end):
+    left = word[start:mid]
+    right = word[mid:end]
+
+    left_index, right_index = 0, 0
+
+    for general_index in range(start, end):
+        if left_index >= len(left):
+            word[general_index] = right[right_index]
+            right_index += 1
+        elif right_index >= len(right):
+            word[general_index] = left[left_index]
+            left_index += 1
+        elif left[left_index] < right[right_index]:
+            word[general_index] = left[left_index]
+            left_index += 1
+        else:
+            word[general_index] = right[right_index]
+            right_index += 1
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # Caso alguma string esteja vazia
+    if first_string == '' or second_string == '':
+        return ("".join(sortword(list(first_string))),
+                "".join(sortword(list(second_string))), False)
+    # Pensar em como refatorar esse return depois
+    first = sortword(list(first_string.lower()))
+    second = sortword(list(second_string.lower()))
+    if first == second:
+        return ("".join(first), "".join(second), True)
+
+    return ("".join(first), "".join(second), False)
+    # raise NotImplementedError
+
+
+# print(is_anagram('ovo', 'vvoo'))
