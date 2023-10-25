f405ac49effdb85aecb62aa9481097956b88c0a1
oliveiraxand <alexandre.osantos253@gmail.com>
2023-10-25 17:09:45 -0300

    feat: requisito 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,16 @@
 def is_palindrome_iterative(word):
     """Faça o código aqui."""
-    raise NotImplementedError
+    n = len(word)
+    count = 0
+    if word == '':
+        return False
+    for i in range(n):
+        # print(word)
+        # print(word[i], word[n - 1 - i])
+        # print(i, n - i - 1, n, count)
+        if word[i] == word[n - i - 1]:
+            count += 1
+            if count == n - 1 or n == 1:
+                return True
+    return False
+    # raise NotImplementedError
